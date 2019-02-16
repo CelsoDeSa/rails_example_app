5a7e4b7845e9fe36577a9e9d8c97805cdf5e51e7
Celso de Sa <730584+CelsoDeSa@users.noreply.github.com>
2019-02-16 13:27:04 -0200

    fixed admin access, it was precedence in routes as I'd imagined

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   resources :posts, param: :slug do 
     resources :books
   end
-
-  get "/:slug", to: "posts#show"  
   
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  get "/:slug", to: "posts#show"
+
   root 'posts#index'
 end
